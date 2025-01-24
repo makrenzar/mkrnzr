2d6bc4c37504c29d7a8923cd1d8ffc3c2e16e500
makrenzjarr <markzaragoza301@gmail.com>
2025-01-24 09:18:45 +0800

    resize image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,13 +7,13 @@ function Header() {
     <nav className="flex justify-center items-center space-x-[3px] p-4 mt-5 mb-5">
       <Link
         to="/"
-        className={`text-xs px-4 py-2 rounded-lg font-bold transition duration-300 hover:text-[#f8f8f8] hover:bg-[#1E1E21] ${location.pathname === '/' ? ' bg-[#1E1E21]' : 'text-[#6D6B6D]'}`}
+        className={`text-xs px-4 py-2 rounded-lg font-bold  hover:text-[#f8f8f8] hover:bg-[#1E1E21] transition-all ${location.pathname === '/' ? ' bg-[#1E1E21]' : 'text-[#6D6B6D]'}`}
       >
         Home
       </Link>
       <Link
         to="/media"
-        className={`text-xs px-4 py-2 rounded-lg font-bold transition duration-300 hover:text-[#f8f8f8] hover:bg-[#1E1E21]  ${location.pathname === '/media' ? ' bg-[#1E1E21]' : 'text-[#6D6B6D]'}`}
+        className={`text-xs px-4 py-2 rounded-lg font-bold hover:text-[#f8f8f8] hover:bg-[#1E1E21] transition-all  ${location.pathname === '/media' ? ' bg-[#1E1E21]' : 'text-[#6D6B6D]'}`}
       >
         Media
       </Link>
